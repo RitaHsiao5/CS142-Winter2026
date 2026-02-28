9227dd851ec508e5bdeca4ae8db6645664ad7d29
Rita Hsiao <chao522957@gmail.com>
2026-02-27 17:27:04 -0800

    Add Zombie.java and NormalZombie.java and SeniorZombie.java to main

---
+++ src/main/java/NormalZombie.java
@@ -0,0 +1,12 @@
+//NormalZombie.java
+
+package src.main.java;
+
+public class NormalZombie extends Zombie{ 
+    
+        public NormalZombie(){
+        //can change these two value  
+        this.damage=15;
+        this.speed=1;
+    }
+}
