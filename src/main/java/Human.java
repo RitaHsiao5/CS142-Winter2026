202eb8408ce0a751be4557e653b11349ba5fe27b
phuongmei <pp.phuongmaibmt@gmail.com>
2026-02-28 16:42:08 -0800

    Update Citizen class

--- src/main/java/Human.java
+++ src/main/java/Human.java
@@ -1,33 +1,38 @@
-public abstract class Human extends LivingEntity {
-    protected boolean infected = false;
+public class Citizen extends Human {
 
-    // Constructor: gọi constructor của LivingEntity
-    public Human(int x, int y, int health) {
+    public Citizen(int x, int y, int health) {
         super(x, y, health);
     }
 
-    // Attack a zombie
-    //public void attack(Zombie z) {
-        // TODO: add attack logic
-    //}
-
-    // Move randomly on the grid
-    public void moveRandom(Entity[][] grid) {
-        // TODO: add move logic
-    }
-
-    // Check infection status
-    public boolean isInfected() {
-        return infected;
-    }
-
-    // Infect this human
-    public void infect() {
-        infected = true;
-    }
-
-    // Step method will be implemented by subclasses (Citizen, Soldier, Doctor, etc.)
     @Override
-    public abstract void step(Entity[][] grid);
+    public void step(Entity[][] grid) {
+        // 1. Move randomly
+        moveRandom(grid);
 
+        // 2. Check nearby humans to spread infection
+        int rows = grid.length;
+        int cols = grid[0].length;
+
+        for (int dx = -1; dx <= 1; dx++) {
+            for (int dy = -1; dy <= 1; dy++) {
+                int nx = getX() + dx;
+                int ny = getY() + dy;
+
+                if (nx >= 0 && nx < rows && ny >= 0 && ny < cols) {
+                    Entity e = grid[nx][ny];
+                    if (e instanceof Human) {
+                        Human h = (Human) e;
+
+                        // If this citizen is infected, chance to infect neighbor
+                        if (this.isInfected() && !h.isInfected()) {
+                            double chance = 0.25; // 25% infection chance
+                            if (Math.random() < chance) {
+                                h.infect();
+                            }
+                        }
+                    }
+                }
+            }
+        }
+    }
 }
