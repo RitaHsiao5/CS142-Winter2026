a1d571ce1ec75c356ae1299bb523fd27062ac349
phuongmei <pp.phuongmaibmt@gmail.com>
2026-03-02 20:19:10 -0800

    Update LordOfZombie

--- src/main/java/LordOfZombie.java
+++ src/main/java/LordOfZombie.java
@@ -2,7 +2,8 @@ package src.main.java;
 
 public class LordOfZombie extends SeniorZombie {
 
-    public LordOfZombie() {
+    public LordOfZombie(int x, int y, int health) {
+        super(x, y, health);
         // Stronger and faster than SeniorZombie
         this.damage = 25;   // more damage
         this.speed = 3;     // faster movement
